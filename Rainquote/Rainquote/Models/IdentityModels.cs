2410b111655d36133bde658c1b702a0d1104d19c
katy <katysorourke@gmail.com>
2016-02-12 12:11:43 -0700

    Support ASP.NET Identity, using Entity Framework 6, connected to MySQL database instead of SQL.

--- Rainquote/Rainquote/Models/IdentityModels.cs
+++ Rainquote/Rainquote/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace Rainquote.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("rainquotedb", throwIfV1Schema: false)
+            : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
 
